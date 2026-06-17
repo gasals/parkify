7b2bd438c62b4f125b4fec491ee13aff4e079857
Sejad Gasal <gasal.sejad@gmail.com>
2026-06-17 23:04:13 +0200

    Reservation migration

---
+++ parkify.Service/Migrations/20260617210244_RemoveReservationQrCodeData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace parkify.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveReservationQrCodeData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QRCodeData",
+                table: "Reservations");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "QRCodeData",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
